021efbd8427475b9ce758b7c8c711df555bea974
cczallen <cczallen@gmail.com>
2020-05-25 16:28:32 +0800

    First commit

---
+++ oTreePractice/settings.py
@@ -0,0 +1,47 @@
+from os import environ
+
+# if you set a property in SESSION_CONFIG_DEFAULTS, it will be inherited by all configs
+# in SESSION_CONFIGS, except those that explicitly override it.
+# the session config can be accessed from methods in your apps as self.session.config,
+# e.g. self.session.config['participation_fee']
+
+SESSION_CONFIG_DEFAULTS = dict(
+    real_world_currency_per_point=1.00, participation_fee=0.00, doc=""
+)
+
+SESSION_CONFIGS = [
+    # dict(
+    #    name='public_goods',
+    #    display_name="Public Goods",
+    #    num_demo_participants=3,
+    #    app_sequence=['public_goods', 'payment_info']
+    # ),
+    dict(
+       name='testing1',
+       display_name="Testing 1",
+       num_demo_participants=3,
+       app_sequence=['testing1']
+    ),
+]
+
+
+# ISO-639 code
+# for example: de, fr, ja, ko, zh-hans
+LANGUAGE_CODE = 'en'
+
+# e.g. EUR, GBP, CNY, JPY
+REAL_WORLD_CURRENCY_CODE = 'USD'
+USE_POINTS = True
+
+ROOMS = []
+
+ADMIN_USERNAME = 'admin'
+# for security, best to set admin password in an environment variable
+ADMIN_PASSWORD = environ.get('OTREE_ADMIN_PASSWORD')
+
+DEMO_PAGE_INTRO_HTML = """ """
+
+SECRET_KEY = 's9^fbh%95pr6r5p+qj=egn04j3mjz@%pod5em18@qdox%qw*-e'
+
+# if an app is included in SESSION_CONFIGS, you don't need to list it here
+INSTALLED_APPS = ['otree']
---
+++ oTreePractice/testing1/models.py
@@ -0,0 +1,42 @@
+from otree.api import (
+    models,
+    widgets,
+    BaseConstants,
+    BaseSubsession,
+    BaseGroup,
+    BasePlayer,
+    Currency as c,
+    currency_range,
+)
+
+
+author = 'Your name here'
+
+doc = """
+Your app description
+"""
+
+
+class Constants(BaseConstants):
+    name_in_url = 'testing1'
+    players_per_group = None
+    num_rounds = 1
+
+
+class Subsession(BaseSubsession):
+    pass
+
+
+class Group(BaseGroup):
+    pass
+
+
+class Player(BasePlayer):
+    are_you_ok = models.BooleanField(
+            label = '你今天好嗎？',
+            choices = [
+                [True, '很好'],
+                [False, '還好']
+            ],
+            widget=widgets.RadioSelect,
+        )
