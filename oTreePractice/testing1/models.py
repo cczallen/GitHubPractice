bfa2307d6335db3e4605e89583aea1c336742f08
elisayuchi <67694102+elisayuchi@users.noreply.github.com>
2020-07-13 19:00:33 +0800

    Step 4: Random Speech Rates

--- oTreePractice/testing1/models.py
+++ oTreePractice/testing1/models.py
@@ -9,6 +9,7 @@ from otree.api import (
     currency_range,
 )
 
+import random
 
 author = 'Your name here'
 
@@ -16,22 +17,34 @@ doc = """
 Your app description
 """
 
+class Subsession(BaseSubsession):
+    def creating_session(self):
+        for p in self.get_players():
+            p.treatment_method = Treatment.get_treatment(p)
+
+class Group(BaseGroup):
+    pass
+
+class Treatment(object):
+    def get_treatment(player):
+        participant = player.participant
+        # lazy loading: 若不存在就決定並起來，若已存在就直接取出
+        if Constants.key_method not in participant.vars:
+            #""" 步驟四：將control跟treatmentWill隨機選擇，存進participant.vars中"""
+            choice = [ 0.7, 0.8, 0.9 ]
+            participant.vars[Constants.key_method] = random.choice(choice)
+        
+        method = participant.vars[Constants.key_method]
+        return method
 
 class Constants(BaseConstants):
     name_in_url = 'testing1'
     players_per_group = None
     num_rounds = 1
-
-
-class Subsession(BaseSubsession):
-    pass
-
-
-class Group(BaseGroup):
-    pass
-
+    key_method = "treatment_method"
 
 class Player(BasePlayer):
+    treatment_method = models.FloatField()
     are_you_ok = models.BooleanField(
             label = '你今天好嗎？',
             choices = [
@@ -40,4 +53,6 @@ class Player(BasePlayer):
             ],
             widget=widgets.RadioSelect,
         )
-    
+
+
+
